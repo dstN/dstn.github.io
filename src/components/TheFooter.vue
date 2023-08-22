5d3d3908a1a12c4bd639ca61590f59a6fff78499
dstN <github@yinside.de>
2023-08-22 23:53:11 +0200

    Fixed Footer and added pub-folder with assets for index.html

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -11,11 +11,11 @@ date = date.getFullYear();
       <div class="column is-half has-text-left">
         Made with
         <a
-          href="https://vuejs.org/"
-          rel="noreferrer"
-          title="Link to Vuejs website"
+          href="https://alvarotrigo.com/fullPage/"
+          rel="nofollow noopener"
+          title="Link to fullPage.js"
           target="_blank"
-          >Vue</a
+          >Vue fullPage</a
         >
         and ❤️ in Hamburg
       </div>
@@ -30,18 +30,14 @@ date = date.getFullYear();
 <style lang="scss" scoped>
 .footer {
   position: absolute;
-  bottom: -3rem;
+  bottom: -2.5rem;
   width: 100%;
   background: transparent;
   border-top: 2px solid transparent;
   padding: 1rem 0;
-  color: lighten(#2b3e50, 20%);
-  @media (min-width: 768px) {
-    padding: 1rem 0;
-  }
+  color: lighten(#2b3e50, 30%);
   @media (min-width: 1024px) {
     padding: 1.5rem 0 3rem;
-    bottom: -5rem;
   }
 }
 </style>
